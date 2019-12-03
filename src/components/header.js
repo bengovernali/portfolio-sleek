f3aaf44c3303154c9b79165a7065d68f7cf84188
Ben Governali <bgovern93@gmail.com>
2019-12-03 15:16:17 -0500

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <div className="title">Ben Governali</div>
+        <div className="links">
+          <a
+            className="link"
+            href="https://github.com/bengovernali"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+          <a
+            className="link"
+            href="https://linkedin.com/in/bengovernali"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Linkedin
+          </a>
+          <div className="link">About</div>
+          <div className="link">Projects</div>
+          <div className="link">Contact</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
