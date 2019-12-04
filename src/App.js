e4c781a3f68df6ac117b168f484df9090bc02849
Ben Governali <bgovern93@gmail.com>
2019-12-04 16:44:23 -0500

    updated app.js for use with react router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Header from "./components/header";
 import Front from "./components/front";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="body">
-          <Header />
-          <div className="main">
-            <Front />
-            <div className="triangle-right"></div>
+      <Router>
+        <div className="App">
+          <div className="body">
+            <Header />
+            <div className="main">
+              <Route path="/" exact component={Front} />
+              <div className="triangle-right"></div>
+            </div>
           </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
