f33cb11d9a1e84211fa180ad10898da9b1178f5c
Ben Governali <bgovern93@gmail.com>
2019-12-05 14:33:14 -0500

    created component and framework for horizontal project card scroll

---
+++ src/components/projects.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Projects extends Component {
+  render() {
+    return (
+      <div className="main">
+        <div className="scroll-wrapper">
+          <div className="project-card"></div>
+          <div className="project-card"></div>
+          <div className="project-card"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Projects;
